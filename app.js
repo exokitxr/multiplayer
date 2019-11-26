c1bb685bcf1aabf0927cb6640cd11206f862ae7a
Avaer Kazmer <a@modules.io>
2019-11-25 23:24:13 -0500

    Bugfix xr-model placeholder model object not having a bounding box mesh

--- app.js
+++ app.js
@@ -574,6 +574,7 @@ class XRModel extends HTMLElement {
     console.log('connected', this, this.getAttribute('src'), this.getAttribute('orientation'), this.getAttribute('scale'));
 
     const model = new THREE.Object3D();
+    model.boundingBoxMesh = new THREE.Object3D();
     container.add(model);
 
     const control = new THREE.TransformControls(camera, renderer.domElement);
